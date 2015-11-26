23a26a227f478a5e653a9ca9403db4a5fee14565
Boris Bera <bboris@rsoft.ca>
2015-11-26 00:03:03 -0500

    Added a basic github client that can fetch users

---
+++ GithubClient.scala
@@ -0,0 +1,29 @@
+package littleprograms
+
+import org.apache.http.client.fluent._
+import org.apache.commons.io.IOUtils
+import java.io.InputStream
+
+case class GithubResponse(status: Int, body: String)
+
+/**
+ * A simple client that makes call to github returning very simple response
+ * objects containing a status code and the whole response string.
+ */
+object GithubClient {
+  private val baseUri = "https://api.github.com"
+
+  /** Fetch a user by name */
+  def user(userName: String) = get(s"$baseUri/users/$userName")
+
+  private def get(uri: String) = execute(Request.Get(uri))
+
+  private def execute(request: Request) = {
+    val response = request.execute().returnResponse()
+
+    GithubResponse(
+      response.getStatusLine().getStatusCode(),
+      IOUtils.toString(response.getEntity().getContent())
+    )
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,11 @@
+lazy val root = (project in file(".")).
+  settings(
+    name := "github-api-monad-examples",
+    version := "0.1",
+    scalaVersion := "2.11.7",
+
+    libraryDependencies ++= Seq(
+      "org.apache.httpcomponents" % "fluent-hc" % "4.5.1",
+      "commons-io" % "commons-io" % "2.4"
+    )
+  )
